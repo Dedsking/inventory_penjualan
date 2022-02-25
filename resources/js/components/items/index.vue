a75c5e2770a758149ee60ffed83b254e26887141
Dedsking <dedyruspandi9@gmail.com>
2022-02-25 17:18:06 +0700

    first commit

---
+++ resources/js/components/items/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+            <div class="col-md-8">
+                <div class="card mt">
+                    <div class="card-header">Data Items</div>
+
+                    <div class="card-body">helow</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log("Component mounted.");
+    },
+};
+</script>
